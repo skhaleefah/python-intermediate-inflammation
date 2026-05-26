02c3fc4e0ea283153f28b7a3a780b83b7183441f
Saqar <s.khaleefah@tudelft.nl>
2026-05-26 11:49:16 +0200

    added test_daily_max function

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.testing as npt
 
 from inflammation.models import daily_mean
+from inflammation.models import daily_max
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +29,15 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers"""
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([5,6])
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
 
