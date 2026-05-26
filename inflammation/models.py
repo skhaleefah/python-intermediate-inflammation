22c735562fee5a059dcf8ae782e665fe1f5d375e
Saqar <s.khaleefah@tudelft.nl>
2026-05-26 14:45:42 +0200

    updated docstring and added typehints in models.py

--- inflammation/models.py
+++ inflammation/models.py
@@ -18,17 +18,29 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+def daily_mean(data:np.array) -> np.array:
+    """ Calculate the daily mean of a 2d inflammation data array
+
+    :param data: a 2D data array with inflammation data (eah row contains measurements for a single patient across all days)
+    :return: an array of mean values of measurements per day
+    """
     return np.mean(data, axis=0)
 
 
-def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+def daily_max(data:np.array) -> np.array:
+    """ Calculate the daily max of a 2d inflammation data array. 
+    
+    :param data: a 2D data array with inflammation data (eah row contains measurements for a single patient across all days)
+    :return: an array of max values of measurements per day 
+    """
     return np.max(data, axis=0)
 
 
-def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+def daily_min(data: np.array) -> np.array:
+    """Calculate the daily min of a 2d inflammation data array
+
+    :param data: a 2D data array with inflammation data (eah row contains measurements for a single patient across all days)
+    :return: an array of min values of measurements per day 
+    """
     return np.min(data, axis=0)
 
