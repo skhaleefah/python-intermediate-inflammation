cd66c07b63d391821ba147134e336404dd83248d
Saqar <s.khaleefah@tudelft.nl>
2026-05-27 09:18:54 +0200

    added file from colleague analysis.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -101,7 +101,15 @@ def test_daily_max(test_input, test_result):
         [
             ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
             ([ [1, 2, -1],[3, -2, 4],[5, -9, 6]], [1,-9,-1]),
+            ([[0, 1, 2], [0, 3, 4]], [0, 1, 2]),     # array containing zeros
+            ([[3, 3, 3], [3, 3, 3], [3, 3, 3]], [3, 3, 3]), # all values the same
         ])
 def test_daily_min(test_input, test_result):
     """Test that min function works for an array of positive and negative integers."""
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_max_nan_propagation():
+    data = np.array([[1, np.nan], [3, 4]])
+    result = daily_max(data)
+    assert np.isnan(result[1])  # documents current behavior
