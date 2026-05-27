70a6b8aac65cc928b75ad01754284453c40885b7
Saqar <s.khaleefah@tudelft.nl>
2026-05-27 11:53:39 +0200

    added csvdatasource in analyse_data and test

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -22,22 +22,12 @@ class CSVDataSource:
         return data
         
 
-# def load_inflammation_data(data_dir):
-#     data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-#     if len(data_file_paths) == 0:
-#         raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-#     data = map(models.load_csv, data_file_paths)
-
-#     return data
-
-
-def analyse_data(data_dir):
+def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_source = CSVDataSource(data_dir)
     data = data_source.load_inflammation_data()
 
     means_by_day = map(models.daily_mean, data)
